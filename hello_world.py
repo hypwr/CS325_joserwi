ab0dcd5599c044a6a42796309d69a9f25d480411
Josh Erwin <joshua.erwin13@gmail.com>
2024-01-21 15:58:20 -0600

    added user's ability to input numbers to be added together

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,8 @@
 print("Hello, this is a basic python script adding 2 + 2.")
 x=2+2
 print("Output:",x)
+print("Now you try, input two numbers and they'll be added together.")
+first = input("First number:")
+second = input("Second number:")
+answer = int(first) + int(second)
+print("Your added number:",answer)
